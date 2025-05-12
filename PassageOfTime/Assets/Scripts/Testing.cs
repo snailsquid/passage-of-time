3247c451e199596f037f463dcb11357913696c98
snailsquid <hexagononyt@gmail.com>
2025-05-13 03:47:02 +0700

    lib: replaced DS with ink

---
+++ PassageOfTime/Assets/Scripts/Testing.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DS;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] DSDialogue ds;
+    void a()
+    {
+    }
+}
